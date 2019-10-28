0819f6f4e1426a551485893ced7dcefacf588c9d
Andrea Fernandes da Graþa Leite Sant'anna <1900269@SALAS.AULAS>
2019-10-28 11:01:11 -0300

    via vs code

---
+++ aplicacao.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/index')
+def index():
+        return render_template ('index.html')
+
+@app.route('/detalhescurso')
+def detalhescurso():
+        return render_template ('detalhescurso.html')
+
+@app.route('/disciplina')
+def disciplina():
+        return render_template ('disciplina.html')
+app.run()
